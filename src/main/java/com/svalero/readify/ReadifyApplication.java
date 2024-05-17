a96b065657e01532c66f60cdb6323becebb82be2
MartaJiGa <103963895+MartaJiGa@users.noreply.github.com>
2024-05-18 00:11:22 +0200

    Initial commit

---
+++ src/main/java/com/svalero/readify/ReadifyApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.readify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadifyApplication.class, args);
+	}
+
+}
