99cb13dc98fd76d36625d1c3be266479a7a8f9b3
MartaJiGa <103963895+MartaJiGa@users.noreply.github.com>
2024-05-18 01:00:57 +0200

    Added @Autowired in BookController and BookService

--- src/main/java/com/svalero/readify/controller/BookController.java
+++ src/main/java/com/svalero/readify/controller/BookController.java
@@ -1,9 +1,14 @@
 package com.svalero.readify.controller;
 
+import com.svalero.readify.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
 
     //region GET
     //endregion
--- src/main/java/com/svalero/readify/service/BookService.java
+++ src/main/java/com/svalero/readify/service/BookService.java
@@ -1,9 +1,14 @@
 package com.svalero.readify.service;
 
+import com.svalero.readify.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
 
     //region GET
     //endregion
