e7985c946ac79a1cb8b81514b7f66557241ed994
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-18 11:54:41 +0200

    Hacer trazas de log en UserService y añadir detalles en el log de BookService

--- src/main/java/com/svalero/readify/service/BookService.java
+++ src/main/java/com/svalero/readify/service/BookService.java
@@ -24,36 +24,44 @@ public class BookService {
         return bookRepository.findAll();
     }
     public Optional<Book> getBookById(Long id) {
-        logger.info("GET /book/{bookId} -> service getBookById()");
+        logger.info("GET /book/{bookId} -> service getBookById(Long id) -> Id: " + id);
         return bookRepository.findById(id);
     }
     public List<Book> findByTitle(String title){
-        logger.info("GET /books -> service findByTitle()");
+        logger.info("GET /books -> service findByTitle(String title) -> Title: " + title);
         return bookRepository.findByTitle(title);
     }
     public List<Book> findByAuthor(String author){
-        logger.info("GET /books -> service findByAuthor()");
+        logger.info("GET /books -> service findByAuthor(String author) -> Author: " + author);
         return bookRepository.findByAuthor(author);
     }
     public List<Book> findByTitleAndAuthor(String title, String author){
-        logger.info("GET /books -> service findByTitleAndAuthor()");
+        logger.info("GET /books -> service findByTitleAndAuthor(String title, String author) -> Title: " + title + ". Author: " + author);
         return bookRepository.findByTitleAndAuthor(title,author);
     }
     //endregion
 
     //region POST
     public void saveBook(Book book){
-        logger.info("POST /books -> service saveBook()");
+        logger.info("ini POST /books -> service saveBook(Book book)" +
+                "\n\tId:" + book.getId() +
+                "\n\tTitle:" + book.getTitle() +
+                "\n\tAuthor:" + book.getAuthor() +
+                "\n\tPublished date:" + book.getPublishedDate() +
+                "\n\tISBN:" + book.getISBN() +
+                "\n\tAvailable:" + book.getAvailable() +
+                "\n\tPages:" + book.getPageCount());
         bookRepository.save(book);
+        logger.info("end POST /books -> service saveBook(Book book)");
     }
     //endregion
 
     //region PUT
     public void modifyBook(Book newBook, long bookId){
 
-        logger.info("ini PUT /book/{bookId} -> service modifyBook() -> findById(bookId)");
+        logger.info("ini PUT /book/{bookId} -> service modifyBook(Book newBook, long bookId) -> findById(bookId) -> BookId: " + bookId);
         Optional<Book> book = bookRepository.findById(bookId);
-        logger.info("end PUT /book/{bookId} -> service modifyBook() -> findById(bookId)");
+        logger.info("end PUT /book/{bookId} -> service modifyBook(Book newBook, long bookId) -> findById(bookId) -> BookId: " + bookId);
 
         if(book.isPresent()){
             Book existingBook = book.get();
@@ -65,18 +73,26 @@ public class BookService {
             existingBook.setAvailable(newBook.getAvailable());
             existingBook.setPageCount(newBook.getPageCount());
 
-            logger.info("ini PUT /book/{bookId} -> service modifyBook() -> save(existingBook)");
+            logger.info("ini PUT /book/{bookId} -> service modifyBook(Book newBook, long bookId) -> save(existingBook) -> BookId: " + bookId);
             bookRepository.save(existingBook);
-            logger.info("end PUT /book/{bookId} -> service modifyBook() -> save(existingBook)");
+            logger.info("end PUT /book/{bookId} -> service modifyBook(Book newBook, long bookId) -> save(existingBook)" +
+                    "\n\tMODIFIED BOOK" +
+                    "\n\tBookId: " + bookId +
+                    "\n\tTitle:" + existingBook.getTitle() +
+                    "\n\tAuthor:" + existingBook.getAuthor() +
+                    "\n\tPublished date:" + existingBook.getPublishedDate() +
+                    "\n\tISBN:" + existingBook.getISBN() +
+                    "\n\tAvailable:" + existingBook.getAvailable() +
+                    "\n\tPages:" + existingBook.getPageCount());
         }
     }
     //endregion
 
     //region DELETE
     public void removeBook(long bookId){
-        logger.info("ini DELETE /book/{bookId} -> service removeBook()");
+        logger.info("ini DELETE /book/{bookId} -> service removeBook(long bookId)");
         bookRepository.deleteById(bookId);
-        logger.info("end DELETE /book/{bookId} -> service removeBook()");
+        logger.info("end DELETE /book/{bookId} -> service removeBook(long bookId)");
     }
     //endregion
 }
--- src/main/java/com/svalero/readify/service/UserService.java
+++ src/main/java/com/svalero/readify/service/UserService.java
@@ -2,6 +2,8 @@ package com.svalero.readify.service;
 
 import com.svalero.readify.domain.User;
 import com.svalero.readify.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,27 +16,43 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
+
     //region GET
     public List<User> getUsers(){
+        logger.info("GET /users -> service getUsers()");
         return userRepository.findAll();
     }
     public List<User> findByName(String name){
+        logger.info("GET /users -> service findByName(String name) -> Name: " + name);
         return userRepository.findByName(name);
     }
     public Optional<User> getUserById(Long id) {
+        logger.info("GET /user/{userId} -> service getUserById(Long id) -> Id: " + id);
         return userRepository.findById(id);
     }
     //endregion
 
     //region POST
     public void saveUser(User user){
+        logger.info("POST /users -> service saveUser(User user) -> " +
+                "\n\tId: " + user.getId() +
+                "\n\tName: " + user.getName() +
+                "\n\tEmail: " + user.getEmail() +
+                "\n\tMembership date: " + user.getMembershipDate() +
+                "\n\tActive: " + user.getActive() +
+                "\n\tRole: " + user.getRole());
         userRepository.save(user);
+        logger.info("POST /users -> service saveUser(User user)");
     }
     //endregion
 
     //region PUT
     public void modifyUser(User newUser, long userId){
+
+        logger.info("ini PUT /user{userId} -> service modifyUser(User newUser, long userId) -> findById(userId) -> UserId: " + userId);
         Optional<User> user = userRepository.findById(userId);
+        logger.info("end PUT /user{userId} -> service modifyUser(User newUser, long userId) -> findById(userId) -> UserId: " + userId);
 
         if(user.isPresent()){
             User existingUser = user.get();
@@ -45,14 +63,18 @@ public class UserService {
             existingUser.setActive(newUser.getActive());
             existingUser.setRole(newUser.getRole());
 
+            logger.info("ini PUT /user{userId} -> service modifyUser(User newUser, long userId) -> save(existingUser) -> UserId: " + userId);
             userRepository.save(existingUser);
+            logger.info("end PUT /user{userId} -> service modifyUser(User newUser, long userId) -> save(existingUser) -> UserId: " + userId);
         }
     }
     //endregion
 
     //region DELETE
     public void removeUser(long userId){
+        logger.info("ini DELETE /user{userId} -> service removeUser(long userId) -> UserId: " + userId);
         userRepository.deleteById(userId);
+        logger.info("end DELETE /user{userId} -> service removeUser(long userId) -> UserId: " + userId);
     }
     //endregion
 }
