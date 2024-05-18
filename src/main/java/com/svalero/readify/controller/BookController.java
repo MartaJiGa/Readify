cafe4b9662a93cc205c9edf5630a5f9c15994925
MartaJiGa <103963895+MartaJiGa@users.noreply.github.com>
2024-05-18 02:24:48 +0200

    CRUD de Book

--- src/main/java/com/svalero/readify/controller/BookController.java
+++ src/main/java/com/svalero/readify/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.svalero.readify.controller;
 
+import com.svalero.readify.domain.Book;
 import com.svalero.readify.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -11,14 +15,35 @@ public class BookController {
     private BookService bookService;
 
     //region GET
+    @GetMapping("/books")
+    public List<Book> findAll(){
+        return bookService.getBooks();
+    }
+    @GetMapping("/book/{bookId}")
+    public Optional<Book> getBook(@PathVariable long bookId) {
+        Optional<Book> book = bookService.getBookById(bookId);
+        return book;
+    }
     //endregion
 
     //region POST
+    @PostMapping("/books")
+    public void saveBook(@RequestBody Book book) {
+        bookService.saveBook(book);
+    }
     //endregion
 
     //region PUT
+    @PutMapping("/book/{bookId}")
+    public void modifyBook(@RequestBody Book book, @PathVariable long bookId) {
+        bookService.modifyBook(book, bookId);
+    }
     //endregion
 
     //region DELETE
+    @DeleteMapping("/book/{bookId}")
+    public void removeBook(@PathVariable long bookId) {
+        bookService.removeBook(bookId);
+    }
     //endregion
 }
--- src/main/java/com/svalero/readify/service/BookService.java
+++ src/main/java/com/svalero/readify/service/BookService.java
@@ -1,8 +1,12 @@
 package com.svalero.readify.service;
 
+import com.svalero.readify.domain.Book;
 import com.svalero.readify.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -11,14 +15,42 @@ public class BookService {
     private BookRepository bookRepository;
 
     //region GET
+    public List<Book> getBooks(){
+        return bookRepository.findAll();
+    }
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
     //endregion
 
     //region POST
+    public void saveBook(Book book){
+        bookRepository.save(book);
+    }
     //endregion
 
     //region PUT
+    public void modifyBook(Book newBook, long bookId){
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(book.isPresent()){
+            Book existingBook = book.get();
+
+            existingBook.setTitle(newBook.getTitle());
+            existingBook.setAuthor(newBook.getAuthor());
+            existingBook.setPublishedDate(newBook.getPublishedDate());
+            existingBook.setISBN(newBook.getISBN());
+            existingBook.setAvailable(newBook.getAvailable());
+            existingBook.setPageCount(newBook.getPageCount());
+
+            bookRepository.save(existingBook);
+        }
+    }
     //endregion
 
     //region DELETE
+    public void removeBook(long bookId){
+        bookRepository.deleteById(bookId);
+    }
     //endregion
 }
