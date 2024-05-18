1e184927efd623c5bc7c7d1707c4cd2d5dde105f
MartaJiGa <103963895+MartaJiGa@users.noreply.github.com>
2024-05-18 04:01:25 +0200

    CRUD de User

---
+++ src/main/java/com/svalero/readify/domain/User.java
@@ -0,0 +1,34 @@
+package com.svalero.readify.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity(name = "author")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column(name = "membership_date")
+    private LocalDate membershipDate;
+
+    @Column
+    private Boolean active;
+
+    @Column
+    private int role;
+}
