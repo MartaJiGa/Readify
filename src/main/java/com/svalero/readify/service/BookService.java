abac579b18de860ab5df49e927a77f0b7c2cf5b2
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-18 11:12:18 +0200

    Hacer trazas de log en BookService

--- src/main/java/com/svalero/readify/service/BookService.java
+++ src/main/java/com/svalero/readify/service/BookService.java
@@ -20,31 +20,40 @@ public class BookService {
 
     //region GET
     public List<Book> getBooks(){
+        logger.info("GET /books -> service getBooks()");
         return bookRepository.findAll();
     }
     public Optional<Book> getBookById(Long id) {
+        logger.info("GET /book/{bookId} -> service getBookById()");
         return bookRepository.findById(id);
     }
     public List<Book> findByTitle(String title){
+        logger.info("GET /books -> service findByTitle()");
         return bookRepository.findByTitle(title);
     }
     public List<Book> findByAuthor(String author){
+        logger.info("GET /books -> service findByAuthor()");
         return bookRepository.findByAuthor(author);
     }
     public List<Book> findByTitleAndAuthor(String title, String author){
+        logger.info("GET /books -> service findByTitleAndAuthor()");
         return bookRepository.findByTitleAndAuthor(title,author);
     }
     //endregion
 
     //region POST
     public void saveBook(Book book){
+        logger.info("POST /books -> service saveBook()");
         bookRepository.save(book);
     }
     //endregion
 
     //region PUT
     public void modifyBook(Book newBook, long bookId){
+
+        logger.info("ini PUT /book/{bookId} -> service modifyBook() -> findById(bookId)");
         Optional<Book> book = bookRepository.findById(bookId);
+        logger.info("end PUT /book/{bookId} -> service modifyBook() -> findById(bookId)");
 
         if(book.isPresent()){
             Book existingBook = book.get();
@@ -56,14 +65,18 @@ public class BookService {
             existingBook.setAvailable(newBook.getAvailable());
             existingBook.setPageCount(newBook.getPageCount());
 
+            logger.info("ini PUT /book/{bookId} -> service modifyBook() -> save(existingBook)");
             bookRepository.save(existingBook);
+            logger.info("end PUT /book/{bookId} -> service modifyBook() -> save(existingBook)");
         }
     }
     //endregion
 
     //region DELETE
     public void removeBook(long bookId){
+        logger.info("ini DELETE /book/{bookId} -> service removeBook()");
         bookRepository.deleteById(bookId);
+        logger.info("end DELETE /book/{bookId} -> service removeBook()");
     }
     //endregion
 }
